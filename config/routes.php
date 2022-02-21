d226aa8e344f4c744d9098e263494fdf92f5c1ae
Izocel <triangle1232009@hotmail.com>
2022-02-21 09:03:31 -0500

    Debut définition route et ajout klke fichiers

--- config/routes.php
+++ config/routes.php
@@ -1,17 +1,36 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
+use Slim\Routing\RouteCollectorProxy;
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', \App\Action\HomeAction::class)->setName('home');
+    $app->get('/', \App\Action\Docs\SwaggerUiAction::class)->setName('home');
 
     // Documentation de l'api
-    $app->get('/docs', \App\Action\Docs\SwaggerUiAction::class);
+    $app->get('/docs', \App\Action\Docs\SwaggerUiAction::class)->setName('docs');
 
-    // Sélection de toutes les épreuves
-    $app->get('/epreuves', \App\Action\Epreuve\EpreuveViewAction::class);
 
+    /**
+     * /    GET     Sélection de toutes les épreuves + body(queryFilter)
+     * /id  GET	    Selection d'une épreuve
+     */
+    $app->group('/epreuves', function (RouteCollectorProxy $group) {
+        $group->get('', \App\Action\Epreuve\EpreuveViewAction::class);
+        $group->get('/{id:[0-9]+}', \App\Action\Epreuve\EpreuveViewAction::class);
+    });
+
+    /**
+     * POST         Insertion d'un résultats
+     */
+    $app->group('/resultats', function (RouteCollectorProxy $group) {
+        $group->post('', \App\Action\Epreuve\EpreuveViewAction::class);
+    });
+
+    /**
+     * /id    PUT     Modification d'un athlete
+     */
+    $app->group('/athletes', function (RouteCollectorProxy $group) {
+        $group->put('/{id:[0-9]+', \App\Action\Epreuve\EpreuveViewAction::class);
+    });
 };
 
